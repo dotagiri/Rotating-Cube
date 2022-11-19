642677a81f2eefef7150eddadfdc2c1cfac350f3
Daisuke Otagiri <daisuotagiri@gmail.com>
2022-11-18 17:57:22 -0800

    added calculate functions for xyz

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 
+int i, j, k;
 
-
-float calcX(){
-     return 
+float calcX(int i, int j, int k){
+     return j*sin(A)*sin(B)*cos(C)-k*cos(A)*sin(B)*cos(C)+j*cos(A)*sin(C)+k*sin(A)*sin(C)+i*cos(B)*cos(C);
 }
 
-float calcY(){
-
+float calcY(int i, int j, int k){
+     return j*cos(A)*cos(C)+k*sin(A)*cos(C)-j*sin(AA)*sin(B)*sin(C)+k*cos(A)*sin(B)*sin(C)-i*cos(B)*sin(C);
 }
 
-float calcZ(){
-
+float calcZ(int i, int j, int k){
+     return k*cos(A)*cos(B)-j*sin(A)*cos(B)+i*sin(B);
 }
 
 int main(){
